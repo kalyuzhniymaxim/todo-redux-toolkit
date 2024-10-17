f03c93504812c09f013cc8350613c639041264cc
Maxim <kalyuzhniy-maxim2017@yandex.ru>
2024-10-17 14:54:35 +0300

    todo-redax

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeTodo, toggleTodoComplete } from '../store/todoSlice';
 
-export const TodoItem = ({ id, text, completed, removeTodo, toggleTodoComplete}) => {
+
+
+
+// const removeItem = () => dispatch(removeTodo({ id }));
+
+export const TodoItem = ({ id, text, completed }) => {
+  const dispatch = useDispatch();
   return (
     <li key={id}>
-      <input
-        type="checkbox"
-        checked={completed}
-        onChange={() => toggleTodoComplete(id)}
-      />
+      <input type="checkbox" checked={completed} onChange={() => dispatch(toggleTodoComplete({id}))} />
       <span>{text}</span>
-      <span className="delete" onClick={() => removeTodo(id)}>
+      <span className="delete" onClick={() => dispatch(removeTodo({ id }))}>
         &times;
       </span>
     </li>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { TodoItem } from './TodoItem';
+import { useSelector } from 'react-redux';
 
-export const TodoList = ({ todos, removeTodo, toggleTodoComplete }) => {
+
+export const TodoList = () => {
+  const todos = useSelector(state => state.todos.todos)
   return (
     <ul>
       {todos.map((todo) => (
         <TodoItem
           key={todo.id}
           {...todo}
-          removeTodo={removeTodo}
-          toggleTodoComplete={toggleTodoComplete}
         />
       ))}
     </ul>
